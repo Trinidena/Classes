0221f3f33f7928c56e09d5426ea61570f4a5a040
Trinidena <deitychomp@yahoo.com>
2022-09-19 13:29:24 -0400

    Create Exceptions.java

---
+++ CMPS_1302_CLASS_WORK/Exceptions.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class EnterDigit
+{
+   public static void main(String[] args)
+   {
+      int digit = 0;
+      
+      while(true)
+      {
+         String digitString = JOptionPane.showInputDialog("Enter a single digit (0-9).");
+      
+         try
+         {
+            digit = Integer.parseInt(digitString);
+            if(digit < 0 || digit > 9)
+            {
+               throw new IllegalArgumentException();
+            }
+            break;
+         }
+         catch(NumberFormatException e)
+         {
+            JOptionPane.showMessageDialog(null , String.format("Invalid input: %s\n" , digitString));
+         }
+         catch(IllegalArgumentException e)
+         {
+            JOptionPane.showMessageDialog(null , String.format("Invalid range: %s\n" , digitString));
+         }
+      }
+      JOptionPane.showMessageDialog(null, String.format("You entered the digit %d.", digit));
+   }   
+}
