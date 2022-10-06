7520c1c61e770d0a8e15eced9b81ce7d8a9f0859
Trinidad M Dena <deitychomp@yahoo.com>
2022-10-05 20:29:47 -0400

    updated

--- Unfinished Work/Assignment6.java
+++ Unfinished Work/Assignment6.java
@@ -1,102 +1,44 @@
-<<<<<<< HEAD
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 import java.io.*;
 
 public class Assignment6
 {
-   public static void main(String[] args) throws FileNotFoundException
+   public static void main(String[] args) throws FileNotFoundException, IOException
    {  
       Scanner in = new Scanner(System.in);
       System.out.println("Enter a file name.");
-      String fileName = in.nextLine(); //
-      System.out.println("Enter as many words as you want. Enter \"end\" to terminate input.");
       
-      String out = ""; //
-      int count = 0;
+      String fileName = in.nextLine(); // 
       
-      String word = in.nextLine();
-         
-      out = out + word;
+      System.out.println("Enter the word you want to count.");
+      
+      String value = in.nextLine(); //
             
-      System.out.println(out);
+      System.out.println("You want to count \"" + value + "\".\n");
             
-      try (//FileWriter output = new FileWriter(fileName); //
-           FileWriter answer = new FileWriter("answer.txt");
+      try (FileWriter answer = new FileWriter("answer.txt");
            FileReader input = new FileReader(fileName);
            BufferedReader br = new BufferedReader(input))
       { 
-         //output.write(out); //
-            
-         String nextCharacter = br.readLine(); ////
-                  
-         while(nextCharacter != null) ////
+         String line = br.readLine();
+         int count = 0;
+         while(line != null)
          {
-            System.out.print(nextCharacter);
-            nextCharacter = br.readLine();
-            count = count++;
+            System.out.print(line);
+            for(int i = 0; i < line.length() - value.length(); i++)
+            {
+               {
+                  if(line.substring(i , i + value.length()).contains(value))
+                  {
+                     count++;
+                  }
+               }
+            }
+            line = br.readLine();
+            System.out.println();
+            System.out.println(value + " has appeared " + count + " time(s)\n");
          }
          answer.write(String.valueOf(count));
       }
-      
-      catch(IOException e)
-      {
-         System.out.printf("IO Exception:  %s%n", e.getMessage());        
-      }
    }
-=======
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import java.io.*;
-
-public class Assignment6
-{
-   public static void main(String[] args) throws FileNotFoundException
-   {  
-      Scanner in = new Scanner(System.in);
-      System.out.println("Enter a file name.");
-      String fileName = in.nextLine(); //
-      System.out.println("Enter as many words as you want. Enter \"end\" to terminate input.");
-      
-      String out = ""; //
-      int count = 0;
-      
-      String word = in.nextLine();
-         
-      out = out + word;
-            
-      System.out.println(out);
-            
-      try (//FileWriter output = new FileWriter(fileName); //
-           FileWriter answer = new FileWriter("answer.txt");
-           FileReader input = new FileReader(fileName);
-           BufferedReader br = new BufferedReader(input))
-      { 
-         //output.write(out); //
-            
-         String nextCharacter = br.readLine(); ////
-                  
-         while(nextCharacter != null) ////
-         {
-            System.out.print(nextCharacter);
-            nextCharacter = br.readLine();
-            count = count++;
-         }
-         answer.write(String.valueOf(count));
-      }
-      
-      catch(IOException e)
-      {
-         System.out.printf("IO Exception:  %s%n", e.getMessage());        
-      }
-   }
->>>>>>> refs/remotes/origin/main
 }
