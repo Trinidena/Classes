f123c8c99b794f31a0894956a8d3a97c0b830e05
Trinidena <deitychomp@yahoo.com>
2022-10-13 14:58:45 -0400

    updated

--- CMPS_2720_CLASS_WORK/LinkdList.java
+++ CMPS_2720_CLASS_WORK/LinkdList.java
@@ -59,50 +59,121 @@ public class LinkdList<E> extends LinkdListAdapter<E>
       return true;
    }
    
+   //Return the node at the given index.
+   
+   private Node getNode(int index)
+   {
+      if(index < 0 || index >= size)
+      {
+         throw new IndexOutOfBoundsException();
+      }
+      Node node = head;
+      for(int i = 0; i < index; i++)
+      {
+         node = node.next;
+      }
+      return node;
+   }
+   
    /* TODO: implement void add(int index, E element) method
             insert element in the list at the given index, moving existing 
             elements to the right */
             
    public void add(int index , E element)
    {
-      if(index < 0 || index >= size)
-      {
-         throw new IndexOutOfBoundsException();
-      }
-      
       if(index == 0)
       {
          head = new Node(element, head);
       }
       else
       {  
-         Node nodeBefore = head;
-         for(int i = 0; i < index - 1; i++)
-         {
-            nodeBefore = nodeBefore.next;
-         }
-            nodeBefore.next = new Node(element, nodeBefore.next);
+         Node nodeBefore = getNode(index - 1);
+         nodeBefore.next = new Node(element, nodeBefore.next);
       }
       size++;
    }
             
    // TODO: implement int size() method returing the size of the list.
    
+   public int size()
+   {
+      return size;
+   }
+   
    // TODO: implement E get(int index) method, returns element at index.
+   
+   public E get(int index)
+   {
+      Node node = getNode(index);
+      return node.element;
+   }
    
    /* TODO: implement E set(int index, E element) method, replace the existing
             value at index with element, return the old value */
+            
+   public E set(int index , E element)
+   {
+      Node node = getNode(index);
+      E oldElement = node.element;
+      node.element = element;
+      return oldElement;
+   }
    
    /* TODO: implement int indexOf(Object o) method, return the index of the 
             first occurrence of o in the list, or -1 if the list does
             not contain the element. */
+            
+   public int indexOf(Object o)
+   {
+      Node node = head;
+      for(int i = 0; i < size; i++)
+      {
+         if(node != null && node.element.equals(o))
+         {
+            return i;
+         }
+         node = node.next;
+      }
+      return -1;
+   }
    
    /* TODO: implement boolean contains(Object o) method, return true of the list
             contains the element. */
+            
+   public boolean contains(Object o)
+   {
+      return indexOf(o) != -1;
+   }
    
    /* TODO: implement E remove(int index) method, remove the element at the 
             the specified index. */
+        
+   public E remove(int index)
+   {
+      E oldElement = get(index);
+      if(index == 0)
+      {
+         head = head.next;
+      }
+      else
+      {
+         Node nodeBefore = getNode(index - 1);
+         nodeBefore.next = nodeBefore.next.next;
+      }
+      return oldElement;
+   }
    
    /* TODO: implement boolean remove(Object o) method, remove the first occurrence of 
             the specified element, */
+            
+   public boolean remove(Object o)
+   {
+      int index = indexOf(o);
+      if(index == -1)
+      {
+         return false;
+      }
+      remove(index);
+      return true;
+   }
 }
