2e47f198423d68b074f40a0d2061cc9860fc172d
Trinidena <deitychomp@yahoo.com>
2022-10-27 16:41:16 -0400

    updated

--- Unfinished Work/PA11.java
+++ Unfinished Work/PA11.java
@@ -10,8 +10,16 @@ public class PA11 extends Application
    public void start(Stage stage)
    {
       BorderPane root = new BorderPane();
+      HBox hBox = new HBox();
+      
+      hBox.setSpacing
       
       Label label = new Label("JavaFX Layouts");
+      Button b0 = new Button("Button 0");
+      Button b1 = new Button("Button 1");
+      Button b2 = new Button("Button 2");
+      TextField t1 = new TextField("TextField 1");
+      TextField t2 = new TextField("TextField 2");
       
       Scene scene = new Scene(root , 600 , 400);
       
