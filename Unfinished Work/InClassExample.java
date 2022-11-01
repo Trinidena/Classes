26e66062b0d0ad0bcf713d3118147720662868ac
Trinidad M Dena <deitychomp@yahoo.com>
2022-11-01 08:47:57 -0400

    updated

--- Unfinished Work/InClassExample.java
+++ Unfinished Work/InClassExample.java
@@ -1,8 +1,9 @@
 import javafx.application.Application;
-import javafx.scene.*;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
+import javafx.geometry.Pos;
 
 public class InClassExample extends Application
 {
