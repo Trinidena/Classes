34a9640c7796ac16a4baaf21d67b127af8d0767b
Trinidad M Dena <deitychomp@yahoo.com>
2022-11-01 08:54:57 -0400

    updated

---
+++ Unfinished Work/InClassExample.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+
+public class InClassExample extends Application
+{
+   public void start(Stage stage)
+   {
+      Pane root = new Pane();
+      
+      Label label1 = new Label("In");
+      Label label2 = new Label("Out");
+      TextField inText = new TextField();
+      TextField outText = new TextField();
+      Button button = new Button("Click Me");
+      
+      root.getChildren().addAll(label1 , label2 , inText , outText , button);
+      inText.relocate(100 , 75);
+      outText.relocate(100 , 125);
+      button.relocate(120 , 175);
+      label1.relocate(70 , 75);
+      label2.relocate(70 , 125);
+      
+      Scene scene = new Scene(root , 400 , 250);
+      stage.setScene(scene);
+      stage.show();
+   }
+}
--- Unfinished Work/PA11.java
+++ Unfinished Work/PA11.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -28,4 +29,28 @@ public class PA11 extends Application
       stage.show();
       
    }
+=======
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
+public class PA11 extends Application
+{
+   public void start(Stage stage)
+   {
+      BorderPane root = new BorderPane();
+      
+      Label label = new Label("JavaFX Layouts");
+      
+      Scene scene = new Scene(root , 600 , 400);
+      
+      stage.setScene(scene);
+      stage.setTitle("Programming Assignment 11");
+      stage.show();
+      
+   }
+>>>>>>> 9e4dbed (updated)
 }
