7798ceb0abb25f3f7077b556f2cf51dfd88462ad
Trinidena <deitychomp@yahoo.com>
2022-11-22 20:01:55 -0500

    Create TestBST.java

---
+++ CMPS_2720_CLASS_WORK/TestBST.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestBST
+{
+   public static void main(String[] args)
+   {
+      BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+      for(int i = 0; i < 20; i++)
+      {
+         Integer newInt = ThreadLocalRandom.current().nextInt(1, 101);
+         tree.insert(newInt);
+      }
+      
+      System.out.println(tree);
+      for(int i = 0; i < 20; i++)
+      {
+         Integer searchInt = ThreadLocalRandom.current().nextInt(1, 101);
+         System.out.println(tree.search(searchInt));
+      }
+      
+         
+   }
+}
