0d966fd396949513c22709f109ad5ae3fab0396d
Trinidad M Dena <deitychomp@yahoo.com>
2023-01-18 09:35:37 -0500

    updated homework

--- CMPS_2313_CLASS_WORK/HW1_Prime_Numbers/PrimeNumberCheckerController.java
+++ CMPS_2313_CLASS_WORK/HW1_Prime_Numbers/PrimeNumberCheckerController.java
@@ -6,73 +6,101 @@ import javafx.scene.control.TextField;
 import java.util.*;
 import java.math.BigInteger;
 import java.lang.StringBuilder;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class PrimeNumberCheckerController {
 
-    @FXML
+    //List<Integer> list = new ArrayList<>();
+   StringBuilder str = new StringBuilder();
+    
+   @FXML
     private TextArea answerTextArea;
 
-    @FXML
+   @FXML
     private Button button;
 
-    @FXML
+   @FXML
     private TextField fromTextField;
 
-    @FXML
+   @FXML
     private TextField toTextField;
 
-    @FXML
+   @FXML
     void handleButton(ActionEvent event) 
-    {
+   {
       primeChecker();
-    }
+      answerTextArea.setText(str.toString());
+      seperateResult();
+   }
 
-    @FXML
+   @FXML
     void handleFromTextField(ActionEvent event) 
-    {
+   {
       primeChecker();
-    }
+      answerTextArea.setText(str.toString());
+      seperateResult();
+   }
 
-    @FXML
+   @FXML
     void handleToTextField(ActionEvent event) 
-    {
+   {
       primeChecker();
-    }
+      answerTextArea.setText(str.toString());
+      seperateResult();
+   }
     
-    public void primeChecker()
-    {
-      String from = fromTextField.getText();
-      int a = Integer.parseInt(from);
-      String to = toTextField.getText();
-      int b = Integer.parseInt(to);
-      
-      //List<Integer> list = new ArrayList<>();
-      StringBuilder str = new StringBuilder();
-      int certainty = 10;
-      int count = 0;
-      
-      for(int i = a + 1; i < b; i++)
+   public void primeChecker()
+   {
+      try
       {
-         BigInteger number = BigInteger.valueOf(i);
-         boolean isPrime = number.isProbablePrime(certainty);
-         
-         if(isPrime == true)
+         String from = fromTextField.getText();
+         int a = Integer.parseInt(from);
+         String to = toTextField.getText();
+         int b = Integer.parseInt(to);
+      
+      
+      
+         int certainty = 10;
+         int count = 0;
+      
+         for(int i = a + 1; i < b; i++)
          {
-            int PrimeNumber = number.intValue();
+            BigInteger number = BigInteger.valueOf(i);
+            boolean isPrime = number.isProbablePrime(certainty);
+         
+            if(isPrime == true)
+            {
+               int PrimeNumber = number.intValue();
             
             //list.add(PrimeNumber);
-            str.append(PrimeNumber + ", ");
-            count++;
-            if(count == 20)
-            {
-               str.append(System.getProperty("line.separator"));
-               count = 0;
+               str.append(PrimeNumber + ", ");
+               count++;
+               if(count == 20)
+               {
+                  str.append(System.getProperty("line.separator"));
+                  count = 0;
+               }
             }
          }
       }
+      catch(NumberFormatException e)
+      {
+         Alert a = new Alert(AlertType.WARNING);
+         a.setHeaderText("Invalid Input");
+         a.setContentText("You have entered an invalid number(s) for your range. Enter a numeric value.\n\n" + 
+                          "Doublecheck your input for any extra space before and after the number");
+         a.show();
+      }
       
       //answerTextArea.setText(Arrays.toString(list.toArray()));
-      answerTextArea.setText(str.toString());
-    }
+      //answerTextArea.setText(str.toString());
+   }
+    
+   public void seperateResult()
+   {
+      str.append(System.getProperty("line.separator"));
+      str.append(System.getProperty("line.separator"));
+   }
 
 }
