09d2b61888a5adf40984b5eec0a32ca3e127b216
Trinidena <deitychomp@yahoo.com>
2023-02-01 16:38:33 -0500

    Add files via upload

--- Unfinished Work/WebView.java
+++ Unfinished Work/WebView.java
@@ -12,10 +12,9 @@ public class WebView extends Application
    {
       FXMLLoader loader = new FXMLLoader(getClass().getResource("WebView.fxml"));
       Parent root = loader.load();
-      Controller controller = loader.getController();
       Scene scene = new Scene(root);
       stage.setScene(scene);
-      stage.setTitle("WebView");
+      stage.setTitle("Web View");
       stage.show();
    }
 }
