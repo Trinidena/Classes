875127ebf0aea3d69d38b6bda1b7a41f8302bd61
Trinidad M Dena <deitychomp@yahoo.com>
2023-02-09 11:59:02 -0500

    WebView browser was updated with various features: history,forward,back,zoom out and in

--- UNFINISHED_WORK/WebView.java
+++ UNFINISHED_WORK/WebView.java
@@ -15,7 +15,7 @@ public class WebView extends Application
       Parent root = loader.load();
       Scene scene = new Scene(root);
       stage.setScene(scene);
-      stage.setTitle("Web View");
+      stage.setTitle("Web Browser");
       stage.show();
    }
 }
